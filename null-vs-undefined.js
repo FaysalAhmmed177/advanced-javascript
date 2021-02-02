61d48b52e01106b6c53e15b12395efdcfa804d2e
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-02-02 11:59:01 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1,num2){
+    console.log(num1+num2);
+}
+
+const result = add(5,6);
+console.log(result);
+
+function add(num1,num2){
+    console.log(num1,num2);
+}
+const result1 = add(5);
+console.log(result1);
+
+const premik = {name:"Smart dude" , phone:1234356};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [23,25,26,27];
+console.log(ages[11]);
