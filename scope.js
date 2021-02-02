2666dfe23abb30ab02de7ed9f397afe79c4e6f89
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-02-02 22:05:12 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,21 @@
+ let bonus = 20;
+ const more = 2;
+function sum(num1 , num2){
+    let result = num1 + num2 + bonus +  more;
+    if(result > 9){
+        let mood = "Happy";
+        mood =  "sad";
+        mood = "Angry";
+        console.log(mood);
+    }
+
+    return result;
+}
+
+const output = sum(5,5);
+// console.log(output);
+// console.log(bonus);
+// console.log(more);
+day = "Friday";
+console.log(day);
+var day;
